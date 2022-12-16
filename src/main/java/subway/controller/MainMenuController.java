985fbaec56d95be8f9c0e0f26243719a5c47810d
jeonghoon <shb03323@naver.com>
2022-12-16 14:38:06 +0900

    feat: 메인 메뉴 선택 기능 추가

--- src/main/java/subway/controller/MainMenuController.java
+++ src/main/java/subway/controller/MainMenuController.java
@@ -1,12 +1,52 @@
 package subway.controller;
 
+import subway.domain.MainMenu;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+import java.util.EnumMap;
+import java.util.Map;
+
 public class MainMenuController {
 
+    private final Map<MainMenu, Controllable> controllers;
+
     public MainMenuController() {
-        initSystem();
+        this.controllers = new EnumMap<>(MainMenu.class);
+        initControllers();
     }
 
-    private void initSystem() {
+    public void run() {
+        initSubway();
+        OutputView.printMainMenu();
+        MainMenu selection = selectMenu();
+        while (!selection.equals(MainMenu.QUIT)) {
+            progress(selection);
+            OutputView.printMainMenu();
+            selection = selectMenu();
+        }
+    }
+
+    private void initControllers() {
+        controllers.put(MainMenu.SEARCH_ROUTE, new SearchRouteController());
+        controllers.put(MainMenu.QUIT, new QuitSystemController());
+    }
+
+    private void initSubway() {
         SubwayInitializer.init();
+    }
+
+    private MainMenu selectMenu() {
+        try {
+            String input = InputView.inputMainMenu();
+            return MainMenu.from(input);
+        } catch (IllegalArgumentException exception) {
+            OutputView.printErrorMessage(exception.getMessage());
+            return selectMenu();
+        }
+    }
+
+    private void progress(MainMenu selection) {
+        controllers.get(selection).runMenu();
     }
 }
--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -4,7 +4,13 @@ import static subway.view.MessagePrefix.OUTPUT_PREFIX;
 
 public class OutputView {
 
-    private static void printMainMenu() {
-        System.out.println(OUTPUT_PREFIX + "메인 화면");
+    public static void printErrorMessage(String message) {
+        System.out.println(message + "\n");
+    }
+
+    public static void printMainMenu() {
+        System.out.println(OUTPUT_PREFIX + "메인 화면\n"
+                + "1. 경로 조회\n"
+                + "Q. 종료\n");
     }
 }
